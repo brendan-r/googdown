9ed5a03d271aa25772374f93a4d3a9c43890c6de
Brendan Rocks <brendan@brendanrocks.com>
2016-12-29 16:02:10 -0800

    [gd_update] Return response contents
    Not httr object

--- R/update.R
+++ R/update.R
@@ -1,19 +1,14 @@
 
 # Upload an md / Rmd file
-gd_update <- function(
-  file_name, file_id, format = defaultUploadFormat()
-) {
-  
-  library(rmarkdown)
-  
-  local_file  <- rmarkdown::render(file_name, get(format)())
-  
+#' @export
+gd_update <- function(local_file, file_id) {
   req <- httr::PATCH(
     paste0("https://www.googleapis.com/upload/drive/v3/files/", file_id),
     httr::config(token = getOption("gd.token")),
     body = httr::upload_file(local_file)
   )
-  
-  req
+
+  httr::stop_for_status(req)
+  httr::content(req)
 }
 
