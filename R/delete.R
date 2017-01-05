f25a760b28acec49caf566b470fe0b7748b3d402
Brendan Rocks <brendan@brendanrocks.com>
2017-01-05 00:02:29 -0800

    [gd_delete | gd_ls] Fix token access

--- R/delete.R
+++ R/delete.R
@@ -1,20 +1,22 @@
 # Delete an md / Rmd file
-gd_delete <- function(file_id, token) {
-  
+#' @export
+gd_delete <- function(file_id) {
+  gd_auth()
+
   req <- httr::DELETE(
     paste0("https://www.googleapis.com/drive/v3/files/", file_id),
-    httr::config(token = token)
+    httr::config(token = getOption("gd.token"))
   )
-  
+
   httr::content(req)
 }
 
 # See the files you have in the drive account
-gd_ls <- function(token) {
+gd_ls <- function() {
   req <- httr::GET(
     paste0("https://www.googleapis.com/drive/v2/files?maxResults=1000"),
-    httr::config(token = token)
+    httr::config(token = getOption("gd.token"))
   )
-  
+
   httr::content(req)
 }
