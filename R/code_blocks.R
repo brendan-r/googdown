54bb28c6de470968790d7952cda1ced60347c15f
Brendan Rocks <brendan@brendanrocks.com>
2017-03-27 19:42:35 -0700

    [code_blocks] Fix misc. wrongness

--- R/code_blocks.R
+++ R/code_blocks.R
@@ -1,5 +1,12 @@
-
+#' @export
 knitr_block_to_pandoc_fenced <- function(lines) {
+  # Fail out if the special characters which you're using to sub for quotes are
+  # in the string
+  if (any(grepl("¶|§", lines))) {
+    stop("Can't used the ascii characters ¶ or § in knitr blocks: Currently ",
+         "used internally by knitr_block_to_pandoc_fenced")
+  }
+
   # Internal function to change a knitr code-block-header to a pandoc one, if
   # detected. Not intended for vecotrized inputs.
   detect_and_change_headers <- function(l) {
@@ -11,14 +18,22 @@ knitr_block_to_pandoc_fenced <- function(lines) {
     if (!is_block_header) return(l)
 
     # Extract the string containing params, and potentially the name
-    params <- l %>% gsub("```[[:space:]]*\\{r,?[[:space:]]+|\\}", "", .) %>%
+    params <- l %>% gsub("```[[:space:]]*\\{r,?[[:space:]]*|\\}", "", .) %>%
       strsplit(",") %>% unlist() %>% gsub("[[:space:]]+", "", .)
 
     # The name is going to be the param which does not feature an equals sign,
     # if there is one. If there's more than one name, throw an informative
     # error.
     name   <- params[!grepl("=", params)]
-    params <- params[grepl("=", params)]
+    params <- params[ grepl("=", params)]
+
+    # All parameters in a pandoc AST code block are strings. Parameters in knitr
+    # code blocks are often numbers (e.g. fig.height) or R varibles. Double
+    # quoting doesn't work. Here using the ascii ¶ symbol to sub for single
+    # quotes, § for double
+    params <- gsub("'", "¶", params)
+    params <- gsub('"', "§", params)
+
 
     if (length(name) > 1L) {
       stop("Bad knitr chunk in .Rmd file: Appears to have more than one name.")
@@ -30,27 +45,29 @@ knitr_block_to_pandoc_fenced <- function(lines) {
     }
 
     # Re-assemble the parts back into pandoc fenced-code-block attributes
-    paste0("```{", name, " .r ", paste(params, collapse = " "), "}")
+    paste0("``` {", name, " .r ", paste(params, collapse = " "), "}")
   }
 
   lines %>% lapply(detect_and_change_headers) %>% unlist()
 }
 
+#' @export
 pandoc_fenced_to_knitr_block <- function(lines) {
   detect_and_change_headers <- function(l) {
     # Is this a pandoc code block/chunk header?
-    is_block_header <- grepl("```\\{.*\\.r.*}[[:space:]]*$", l)
+    is_block_header <- grepl("```[[:space:]]*\\{.*\\.r.*}[[:space:]]*$", l)
 
     # If not, return the original input
     if (!is_block_header) return(l)
 
     # Look for a word starting with #
-    name <- str_extract(l, "#[[:alnum:]]*") %>% gsub("#", "", .) %>%
+    name <- stringr::str_extract(l, "#[[:alnum:][:punct:]]*") %>%
+      gsub("#", "", .) %>%
       na.omit()
 
     params <- l %>%
       # Remove everything that isn't a param
-      gsub("```\\{|}|#[[:alnum:]]+|\\.r", "", .) %>%
+      gsub("```[[:space:]]*\\{|}|#[[:alnum:][:punct:]]+|\\.r", "", .) %>%
       # Remove spaces around equals signs
       gsub("[[:space:]]*=[[:space:]]*", "=", .) %>%
       # Remove unneccesary white space
@@ -60,9 +77,23 @@ pandoc_fenced_to_knitr_block <- function(lines) {
       # Split on spaces
       strsplit(" ") %>% unlist
 
-    paste0("```{r ", paste0(c(name, params), collapse = ", "), "}")
+    # Because pandoc doesn't accept strings as fenced code-block arguments, we
+    # replace single/double quotes with the ascii symbols ¶/§ in
+    # `knitr_block_to_pandoc_fenced`. Sub them back now. out now
+    params       <- gsub('"', '',  params)
+    params       <- gsub('¶', "'", params)
+    params       <- gsub('§', '"', params)
+
+    param_string <- paste0(c(name, params), collapse = ", ")
+
+    # If the param string is non-empty, prefix it with a space. This is so that
+    # you either get ```{r params} or ```{r} (no space before closing bracket)
+    if (nchar(param_string) > 0) {
+      param_string <- paste0(" ", param_string)
+    }
+
+    paste0("```{r", param_string, "}")
   }
 
   lines %>% lapply(detect_and_change_headers) %>% unlist()
 }
-
