77bd0b70e4afb3ff96fb081a33ae7cca3caea4e0
Brendan Rocks <brendan@brendanrocks.com>
2017-05-16 20:31:03 -0700

    [tests] Add tests for code blocks

---
+++ tests/testthat/test_04_code_blocks.R
@@ -0,0 +1,40 @@
+context("Code Blocks")
+
+test_that("Code block changes are reversible", {
+
+  cb_test_files <- normalizePath(list.files(
+    "../test_files/code_blocks//", full.names = T
+  ))
+
+  for (f in cb_test_files) {
+
+    original_lines <- readLines(f)
+
+    new_lines <- pandoc_fenced_to_knitr_block(
+      knitr_block_to_pandoc_fenced(original_lines)
+    )
+
+    # Is it the same?
+    expect_equal(original_lines, new_lines)
+  }
+
+})
+
+
+test_that("Code block translations can be understood by Pandoc", {
+
+  for (f in cb_test_files) {
+
+    original_lines <- readLines(f)
+
+    tf1 <- tempfile()
+    tf2 <- tempfile()
+
+    writeLines(knitr_block_to_pandoc_fenced(original_lines), tf1)
+
+    system(paste("pandoc -f markdown -t json", tf1, "-o", tf2))
+
+    expect_true(any(grepl("CodeBlock", readLines(tf2))))
+  }
+
+})
