d19b9126a8fef01bd8bae6539062bf9ece8b393d
Brendan Rocks <brendan@brendanrocks.com>
2017-06-20 22:01:57 -0700

    [file_path] Rm docstrings
    They seem to upset R CMD CHECK for some reason

--- R/misc.R
+++ R/misc.R
@@ -6,15 +6,6 @@ validateYaml <- function(file) {
 }
 
 
-#' Construct Path to File
-#'
-#' A modified version of \code{\link{file.path}}, which prevents \code{fsep}
-#' cropping up more than once.
-#'
-#' @param ... character vectors
-#' @param fsep the path separator to use.
-#'
-#' @return A character vector
 file_path <- function(..., fsep = .Platform$file.sep) {
   # Do the file.path thing
   p <- file.path(..., fsep = fsep)
